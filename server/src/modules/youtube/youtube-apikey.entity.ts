fbb193b161de61e88e88ab6e10f592083641c7b1
5ivecms <greatseo@yandex.ru>
2023-09-17 08:27:54 +0300

    keywords createdAt column

--- server/src/modules/youtube/youtube-apikey.entity.ts
+++ server/src/modules/youtube/youtube-apikey.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from 'typeorm'
 
 @Entity('youtube-apikey')
 export class YoutubeApikey {
@@ -22,4 +22,7 @@ export class YoutubeApikey {
 
   @Column({ type: 'varchar', default: '' })
   public comment?: string
+
+  @CreateDateColumn({ type: 'timestamptz' })
+  public createdAt: Date
 }
